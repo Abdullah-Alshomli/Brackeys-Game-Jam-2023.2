1461e21b74bc1f787881dd923f046e7ddce6d14e
Abdullah Alshomli <shooana444@gmail.com>
2023-08-26 03:30:48 +0300

    added fall damage

---
+++ Brackeys Game Jam 2023.2 Game/Assets/Scripts/FallDamage.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDamage : MonoBehaviour
+{
+    public float fallDamageThreshold = 10f;
+    public float fallDamageMultiplier = 1f;
+    private HPComponent playerHP;
+    private void Start()
+    {
+        playerHP = GetComponent<HPComponent>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.y > fallDamageThreshold && collision.gameObject.layer != Layers.Enemis)
+        {
+            float damage = (collision.relativeVelocity.y - fallDamageThreshold) * fallDamageMultiplier;
+            playerHP.TakeDamage((int)damage);
+            Debug.Log("Player took " + (int)damage + " damage from falling.");
+        }
+    }
+}
